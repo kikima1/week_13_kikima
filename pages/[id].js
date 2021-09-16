4d3b9db34ab422b81fc899d90c10db39122841c1
Miriam Block <kikima@mac.com>
2021-09-16 00:38:42 +0000

    changes incorporating a link

--- pages/[id].js
+++ pages/[id].js
@@ -2,18 +2,22 @@ import Head from 'next/head';
 import Layout from '../components/layout';
 import { getAllIds, getData } from '../lib/data';
 
+
 export async function getStaticProps({ params }) {
   const itemData = await getData(params.id);
+  //const itemData1 = await getData(params.name);
   // console.log(itemData);
   return {
     props: {
-      itemData
+      itemData,
+     
     }
   };
 }
 
 export async function getStaticPaths() {
   const paths = getAllIds();
+  
   return {
     paths,
     fallback: false
@@ -21,19 +25,22 @@ export async function getStaticPaths() {
 }
 
 //added extra item data from list one which had changed data from second JSON object 
-export default function Entry({ itemData }) {
+export default function Entry({ itemData}) {
+  
   return (
+    
     <Layout>
+    
       <article className="card col-6">
         <div className="card-body">
           <h5 className="card-title">{itemData.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
           <p className="card-text">{itemData.birthdate}</p>
-          <p className="card-text">{itemData.name2}</p>
+           <p> <a href={itemData.email2} className="card-text">{itemData.name2}</a> from List 2.</p> 
           <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
         </div>
       </article>
-    </Layout>
-  );
- 
-}
+      </Layout>
+  )}
+  //Tried to make a URL with name appended work by having a get static paths directory in next indexed to names and ids but didn't work.
+   // <p> <a href={itemData.name2} className="card-text">{itemData.name2}//</a> from List 2.</p> 
