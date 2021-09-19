3d7f02ae2ce1a1690289dbe51265cddab079a94d
Miriam Block <kikima@mac.com>
2021-09-19 18:28:30 +0000

    first commit

--- pages/[id].js
+++ pages/[id].js
@@ -1,23 +1,20 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Layout from '../components/layout';
 import { getAllIds, getData } from '../lib/data';
 
-
 export async function getStaticProps({ params }) {
   const itemData = await getData(params.id);
-  //const itemData1 = await getData(params.name);
   // console.log(itemData);
   return {
     props: {
-      itemData,
-     
+      itemData
     }
   };
 }
 
 export async function getStaticPaths() {
   const paths = getAllIds();
-  
   return {
     paths,
     fallback: false
@@ -25,22 +22,57 @@ export async function getStaticPaths() {
 }
 
 //added extra item data from list one which had changed data from second JSON object 
-export default function Entry({ itemData}) {
-  
+export default function Entry({ itemData }) {
   return (
-    
     <Layout>
-    
       <article className="card col-6">
+      <h2>Person Details</h2>
         <div className="card-body">
           <h5 className="card-title">{itemData.name}</h5>
           <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
           <p className="card-text">{itemData.birthdate}</p>
-           <p> <a href={itemData.email2} className="card-text">{itemData.name2}</a> from List 2.</p> 
           <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
         </div>
       </article>
-      </Layout>
-  )}
-  //Tried to make a URL with name appended work by having a get static paths directory in next indexed to names and ids but didn't work.
-   // <p> <a href={itemData.name2} className="card-text">{itemData.name2}//</a> from List 2.</p> 
+      <h2> Best Friend</h2>
+      <div className="list-group col-6">
+      {itemData.best ?
+      itemData.best.map(
+        ({id, name})=>(
+          <Link key={id} href={`/${id}`}>
+          <a className= "list-group-item list-group-item-action">{name}</a>
+          </Link>
+        )
+      )
+      :null
+      }
+      </div>
+      <h2> Related Persons</h2>
+      <div className="list-group col-6">
+      {itemData.related ?
+      itemData.related.map(
+        ({id, name})=>(
+          <Link key={id} href={`/${id}`}>
+          <a className= "list-group-item list-group-item-action">{name}</a>
+          </Link>
+        )
+      )
+      :null
+      }
+      </div>
+       <h2> More Related Persons from Second JSON object</h2>
+      <div className="list-group col-6">
+      {itemData.more_related ?
+      itemData.more_related.map(
+        ({id, name})=>(
+          <Link key={id} href={`/${id}`}>
+          <a className= "list-group-item list-group-item-action">{name}</a>
+          </Link>
+        )
+      )
+      :null
+      }
+      </div>
+    </Layout>
+  );
+}
